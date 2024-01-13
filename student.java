17a721d4c250c7fa2b71c19a8b20a23a3c9fa43f
Sanket <sanket@242422@gmail.com>
2024-01-13 11:36:38 +0530

    student update

---
+++ student.java
@@ -0,0 +1,9 @@
+public class student {
+    public student(){
+        System.out.println("creating student object");
+    }
+
+    public void sayHello(){
+        System.out.println("hi , iam a student");
+    }
+}
